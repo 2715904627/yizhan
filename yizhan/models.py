0e8d2f5b462a8184cd49e9d4469604193cbd16b2
Kadupul <2715904627@qq.com>
2023-03-17 09:03:38 +0800

    add user table

--- yizhan/models.py
+++ yizhan/models.py
@@ -8,3 +8,19 @@ class Yizhan(models.Model):
     NQ_en = models.TextField("Question_en")
     NA_ru = models.TextField("Answer_ru")
     NQ_ru = models.TextField("Question_ru")
+
+
+
+class User(models.Model):
+    username = models.CharField('用户名', max_length=50, default='', unique=True)
+    email = models.EmailField('邮箱', null=True)
+    firstname = models.CharField('名', max_length=50, default='',null=True)
+    lasttname = models.CharField('姓', max_length=50, default='',null=True)
+    password = models.CharField('密码', max_length=32)
+    phone = models.CharField('手机号', max_length=20,null=True)
+    addr1 = models.CharField('地址', max_length=50,null=True)
+    addr2 = models.CharField('备用地址', max_length=50,null=True)
+    city = models.CharField('城市', max_length=20,null=True)
+    country = models.CharField('国家', max_length=50,null=True)
+    state = models.CharField('州', max_length=50,null=True)
+    zip = models.CharField('邮编', max_length=20,null=True)
