cc4de825d67d1595e9d400cd7f13b0f4eb783f06
Kadupul <2715904627@qq.com>
2023-03-17 08:57:10 +0800

    create table

--- yizhan/models.py
+++ yizhan/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Yizhan(models.Model):
+    NA_zh = models.TextField("Answer_zh")
+    NQ_zh = models.TextField("Question_zh")
+    NA_en = models.TextField("Answer_en")
+    NQ_en = models.TextField("Question_en")
+    NA_ru = models.TextField("Answer_ru")
+    NQ_ru = models.TextField("Question_ru")
