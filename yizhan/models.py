5966830c93a5ae318fbe920b6ac001c2a39881a0
Kadupul <2715904627@qq.com>
2023-03-17 09:10:45 +0800

    enable null

--- yizhan/models.py
+++ yizhan/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Yizhan(models.Model):
-    NA_zh = models.TextField("Answer_zh")
-    NQ_zh = models.TextField("Question_zh")
-    NA_en = models.TextField("Answer_en")
-    NQ_en = models.TextField("Question_en")
-    NA_ru = models.TextField("Answer_ru")
-    NQ_ru = models.TextField("Question_ru")
+    NA_zh = models.TextField("Answer_zh",null=True)
+    NQ_zh = models.TextField("Question_zh",null=True)
+    NA_en = models.TextField("Answer_en",null=True)
+    NQ_en = models.TextField("Question_en",null=True)
+    NA_ru = models.TextField("Answer_ru",null=True)
+    NQ_ru = models.TextField("Question_ru",null=True)
 
     class Meta:
         db_table = 'Yizhan'
